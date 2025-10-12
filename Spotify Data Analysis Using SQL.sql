5151e6e256bd1426340798093eb8849a47643478
Yashu1411 <yashwithasiyadri@gmail.com>
2025-10-12 08:54:10 +0530

    Update Spotify Data Analysis Using SQL.sql

--- Spotify Data Analysis Using SQL.sql
+++ Spotify Data Analysis Using SQL.sql
@@ -51,14 +51,6 @@ SELECT * FROM spotify
 WHERE duration_min=0;
 
 
-/*
-Retrieve the names of all tracks that have more than 1 billion streams.
-List all albums along with their respective artists.
-Get the total number of comments for tracks where licensed = TRUE.
-Find all tracks that belong to the album type single.
-Count the total number of tracks by each artist.
-*/
-
 
 --Retrieve the names of all tracks that have more than 1 billion streams.
 SELECT * FROM spotify
@@ -87,16 +79,6 @@ FROM spotify
 GROUP BY artist
 ORDER BY 2 DESC;
 
-
--------LEVEL 2------
-/*
-Calculate the average danceability of tracks in each album.
-Find the top 5 tracks with the highest energy values.
-List all tracks along with their views and likes where official_video = TRUE.
-For each album, calculate the total views of all associated tracks.
-Retrieve the track names that have been streamed on Spotify more than YouTube.
-
-*/
 
 --Calculate the average danceability of tracks in each album.
 SELECT
@@ -150,11 +132,6 @@ WHERE
 	AND 
 	streamed_on_youtube <> 0
 
-/*
-Find the top 3 most-viewed tracks for each artist using window functions.
-Write a query to find tracks where the liveness score is above the average.
-Use a WITH clause to calculate the difference between the highest and lowest energy values for tracks in each album.
-*/
 --Find the top 3 most-viewed tracks for each artist using window functions.
 
 WITH ranking_artist
